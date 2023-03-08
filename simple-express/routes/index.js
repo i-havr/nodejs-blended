dac77517f1e41468ab132119b98bb23f64dc82d2
i-havr <i.havrylov@gmail.com>
2023-03-08 11:32:04 +0200

    fix

--- simple-express/routes/index.js
+++ simple-express/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/:id', function (req, res, next) {
+  const { id } = req.params;
+  const contact = contacts.filter(el => el.id === id);
+  res.json(contact);
+});
+
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+  res.render('response', { title: 'Simple express app', email, password });
 });
 
 module.exports = router;
--- simple-express/routes/users.js
+++ simple-express/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const contacts = [
+  { id: '1', username: 'Felix', surname: 'Brown', email: 'felix@test.com' },
+  { id: '2', username: 'Sonya', surname: 'Redhead', email: 'sonya@test.com' },
+  { id: '3', username: 'Conan', surname: 'Barbarian', email: 'conan@test.com' },
+];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send(contacts);
 });
 
 module.exports = router;
